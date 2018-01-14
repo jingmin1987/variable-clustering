a4639b46c7ff92fb99f4e4717dccf55fda063fab
Jingmin Zhang <jingmin1987@gmail.com>
2018-01-14 14:22:52 -0500

    package defined

---
+++ decomposition/base_class.py
@@ -0,0 +1,17 @@
+# Define base class for all decomposition classes
+
+import abc
+
+
+class BaseDecompositionClass:
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def fit(self, dataframe):
+        """
+
+        :param dataframe: a pandas dataframe that includes feature space to be decomposed
+        :return:
+        """
+
+        pass
---
+++ decomposition/var_clus.py
@@ -0,0 +1,10 @@
+# Definition for class VarClus
+
+from decomposition.base_class import BaseDecompositionClass
+
+
+class VarClus(BaseDecompositionClass):
+    def __init__(self):
+        pass
+
+    
