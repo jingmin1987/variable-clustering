6071097a6caf3d88d8e50548adc3a61455eb9ac5
Jingmin Zhang <jingmin1987@gmail.com>
2018-01-14 15:15:02 -0500

    more class definition

--- decomposition/var_clus.py
+++ decomposition/var_clus.py
@@ -1,9 +1,37 @@
 # Definition for class VarClus
 
+import pandas as pd
+from sklearn.decomposition import PCA
+
 from decomposition.base_class import BaseDecompositionClass
 
 
-class VarClus(BaseDecompositionClass):
-    def __init__(self):
-        pass
+class Cluster:
+    """
+    A container that holds the following information
+        - features in this cluster
+        - first n PCA components and their corresponding eigenvalues
+    """
 
+    def __init__(self,
+                 dataframe,
+                 n_split=2,
+                 features=None):
+
+        self.features = features or dataframe.columns
+        self.dataframe = dataframe[features]
+        self.n_split = n_split
+
+    def run_pca(self):
+        self.pca = PCA(n_components=self.n_split)
+        self.pca = self.pca.fit(self.dataframe)
+
+
+class VarClus(BaseDecompositionClass):
+    def __init__(self,
+                 n_split=2):
+
+        self.n_split = n_split
+
+    def fit(self, dataframe):
+        pass
